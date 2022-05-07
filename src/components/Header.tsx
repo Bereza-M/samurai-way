f3be58f7b0f74bc028b6dee3ff07622b87d099b4
MichailBereznev <97397066+Bereza-M@users.noreply.github.com>
2022-05-07 20:47:16 +0300

    07.05

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Header() {
+    return (
+        <header className='header'>
+        <img src='https://assets.turbologo.com/blog/en/2019/11/19084834/gaming-logo-cover.jpg' alt='logo'/>
+    </header>
+    );
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export function Profile() {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src='https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg'
+                    alt='back'/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
