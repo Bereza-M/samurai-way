ff278d4ed3b61b34875194e7409e68ef15931f90
MichailBereznev <97397066+Bereza-M@users.noreply.github.com>
2022-06-06 00:05:58 +0300

    06.06

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 export function Navbar() {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href="/profile">Profile</a>
+                <NavLink to='/Profile' className={navData=>navData.isActive ? s.active : s.item }>Profile</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs" className={navData=>navData.isActive ? s.active : s.item}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/news">News</a>
+                <NavLink to="/news" className={navData=>navData.isActive ? s.active : s.item}>News</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/music">Music</a>
+                <NavLink to="/music" className={navData=>navData.isActive ? s.active : s.item}>Music</NavLink>
             </div>
-            <div className={s.item} >
-                <a href="/settings">Settings</a>
+            <div className={s.item}>
+                <NavLink to="/settings" className={navData=>navData.isActive ? s.active : s.item}>Settings</NavLink>
             </div>
         </nav>
     );
